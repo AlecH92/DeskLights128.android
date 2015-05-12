69a502b0a135ac8d0b0a3fce9e240c9113ce7c05
Alec Harley <alec.91@gmail.com>
2015-05-12 13:27:18 -0700

    Updated Wear Listener to not crash
    Wear listener would crash app upon sending if the app was not in recents
    because it tried to pull the IP address from MainActivity and not from
    sharedPreferences.
    App updated to V4.1/code12 for publish to store.

--- desklights128.phone/src/main/java/com/hilltoprobotics/desklights128/phone/ListenerService.java
+++ desklights128.phone/src/main/java/com/hilltoprobotics/desklights128/phone/ListenerService.java
@@ -1,6 +1,8 @@
 package com.hilltoprobotics.desklights128.phone;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -8,6 +10,8 @@ import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class ListenerService extends WearableListenerService {
+    public static SharedPreferences sharedPrefs;
+    String IPAddress = "";
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
@@ -24,10 +28,13 @@ public class ListenerService extends WearableListenerService {
         Log.v("blue", String.valueOf(theColorBlue));
         sendData("color?r="+theColorRed+"&g="+theColorGreen+"&b="+theColorBlue);
         Toast.makeText(this, "Wear Sent " + message, Toast.LENGTH_LONG).show();
+        Log.d("DL128.phone","Wear Sent: " + message);
     }
 
     void sendData(String theData) {
-        String url = "http://" + MainActivity.IPAddress + "/" + theData;
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        IPAddress = sharedPrefs.getString("prefIP", "127.0.1.1");
+        String url = "http://" + IPAddress + "/" + theData;
         final ThreadedRequest tReq = new ThreadedRequest(url);
         tReq.start(new Runnable() {
             public void run() {
